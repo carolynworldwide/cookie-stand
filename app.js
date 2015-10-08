e071832b61cda53786ec7384039a3cc27c6f18d8
carolynworldwide <carolynworldwide@gmail.com>
2015-10-08 16:38:50 -0700

    data grabbing

--- app.js
+++ app.js
@@ -10,41 +10,42 @@ var CookieStand = function Store(place, minCustHour, maxCustHour, avgCookiesCust
   this.randCustHour = function() {
     return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour; 
   };
+  
   this.totalCookiesHour = function() {
     return Math.floor(this.randCustHour() * this.avgCookiesCust);
   };
+  
   this.totalCookiesDay = function() {
-      var dailyCookies = 0; 
-      for(var i = 0; i < 8; i++) {
-        this.cookiesByHourList.push(this.totalCookiesHour());
-        dailyCookies = dailyCookies + this.totalCookiesHour();
-        console.log (i+10);
-        console.log (dailyCookies);
-      }
+    var dailyCookies = 0; 
+    for(var i = 0; i < 8; i++) {
+      this.cookiesByHourList.push(this.totalCookiesHour());
+      dailyCookies = dailyCookies + this.totalCookiesHour();
+     //console.log (i+10);
+     // console.log (dailyCookies);
+    }
     return dailyCookies;
   };
+    
   this.makeUL = function() {
     this.totalCookiesDay(); 
      // connect the list to the HTML; this is where your list will appear
     var awesomeList = document.getElementById(this.domName);
     
     for(var i = 0; i < this.cookiesByHourList.length; i++) {
-         // Create the list item:
-        var item = document.createElement('li');
-        // Set its contents:
-        item.appendChild(document.createTextNode((i+10) + ':00 ' + this.cookiesByHourList[i] + ' cookies'));
-        // Add it to the list:
-        awesomeList.appendChild(item);
-   }
-        var item = document.createElement('li');
-
-        item.appendChild(document.createTextNode('Total: ' + this.totalCookiesDay() + ' cookies'));
-
-        awesomeList.appendChild(item);
-
-        return awesomeList;
+       // Create the list item:
+      var item = document.createElement('li');
+      // Set its contents:
+      item.appendChild(document.createTextNode((i+10) + ':00 ' + this.cookiesByHourList[i] + ' cookies'));
+      // Add it to the list:
+      awesomeList.appendChild(item);
+    }
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode('Total: ' + this.totalCookiesDay() + ' cookies'));
+    awesomeList.appendChild(item);
+    return awesomeList;
   };
-};
+}
+
 
 var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2, 'pi');
 var seaTac = new CookieStand('SeaTac Airport', 6, 44, 1.2, 'se');
@@ -61,4 +62,17 @@ bellevueSquare.makeUL();
 alki.makeUL();
 northgate.makeUL();
 tacoma.makeUL();
+
+var addButton = document.getElementById('addButton');
+
+var handleAddButton = function(event) {
+  event.preventDefault();
+  var place = document.getElementById('place').value;
+  var minCust = document.getElementById('minCust').value;
+  var maxCust = document.getElementById('maxCust').value;
+  var avCookies = document.getElementById('avCookies').value;
+  console.log(place, minCust, maxCust, avCookies);
+};
+
+addButton.addEventListener('click', handleAddButton);
 

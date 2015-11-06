e96a15cb419e781cb7e7b0a6f441de7acd078f43
carolynworldwide <carolynworldwide@gmail.com>
2015-11-06 09:26:00 -0800

    updated code

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-'use strict';//don't necessarily have to use this
-
-var CookieStand = function Store(place, minCustHour, maxCustHour, avgCookiesCust, domName)//different variables to call up with this function) 
+var CookieStand = function(place, minCustHour, maxCustHour, avgCookiesCust)
 {
-  this.place = place;//variable defined within this function, NAME OF STORE 
-  this.minCustHour = minCustHour;//variable defined within this function, RANDOM GENERATED #
-  this.maxCustHour = maxCustHour;//variable defined within this function, RANDOM GENERATED #
-  this.avgCookiesCust = avgCookiesCust;//variable defined within this function
-  this.cookiesByHourList = [];//variable defined within this function
-  this.domName = domName;//variable defined within this function
+  this.place = place;
+  this.minCustHour = minCustHour;
+  this.maxCustHour = maxCustHour;
+  this.avgCookiesCust = avgCookiesCust;
+  this.cookiesByHourList = [];
 
   this.randCustHour = function() {
     return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour +1)) + this.minCustHour; 
-  };
-  
+  };  
   this.totalCookiesHour = function() {
     return Math.floor(this.randCustHour() * this.avgCookiesCust);
   };
-  
   this.getCookiesPerHour = function() {
     for(var i = 0; i < 8; i++) {
         this.cookiesByHourList.push(this.totalCookiesHour());
     }
   };
-
   this.totalCookiesDay = function() {
     var dailyCookies = 0; 
     for(var i = 0; i < this.cookiesByHourList.length; i++) {
@@ -30,24 +24,26 @@ var CookieStand = function Store(place, minCustHour, maxCustHour, avgCookiesCust
     }
     return dailyCookies;
   };
-    
-  this.makeTable = function() {
+  this.makeTableRow = function() {
     var tab = document.getElementById('table');
     var tr = document.createElement('tr');
-    tr.textContent = this.place;
-    tab.appendChild(tr);
+    var td = document.createElement('td');
+    td.textContent = this.place;
+    tr.appendChild(td);
+
     for (var j = 0; j < this.cookiesByHourList.length; j++) {
-      var td = document.createElement('td');
+      td = document.createElement('td');
       td.textContent = this.cookiesByHourList[j];
       tr.appendChild(td);
     }
     var tdTotal = document.createElement('td');
     tdTotal.textContent = this.totalCookiesDay();
     tr.appendChild(tdTotal);
+    tab.appendChild(tr);  
   };
-  
+
   this.getCookiesPerHour();
-  this.makeTable();
+  this.makeTableRow();
 }
 
 var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2, 'pi');
@@ -58,15 +54,20 @@ var alki = new CookieStand('Alki', 3, 24, 2.6, 'al');
 var northgate = new CookieStand('Northgate', 12, 44, 3.6, 'no');
 var tacoma = new CookieStand('Tacoma', 5, 9, 20.7, 'ta');
 
-var addButton = document.getElementById('addButton');
+var addForm = document.getElementById('addForm');
 
-var handleAddButton = function(event) {
+var handleAddForm = function(event) {
   event.preventDefault();
-  var place = document.getElementById('place').value;
-  var minCust = document.getElementById('minCust').value;
-  var maxCust = document.getElementById('maxCust').value;
-  var avCookies = document.getElementById('avCookies').value;
-  //console.log(place, minCust, maxCust, avCookies);
+  if (!event.target.place.value || !event.target.minCust.value || !event.target.maxCust.value || !event.target.avCookies.value) {
+    return alert('no empty fields allowed');
+  }
+var newPlace = new CookieStand(event.target.place.value, event.target.minCust.value, event.target.maxCust.value, event.target.avCookies.value);
+console.log(newPlace);
+
+event.target.place.value = null;
+event.target.minCust.value = null;
+event.target.maxCust.value = null;
+event.target.avCookies.value = null;
 };
 
-addButton.addEventListener('click', handleAddButton);
+addForm.addEventListener('submit', handleAddForm);
